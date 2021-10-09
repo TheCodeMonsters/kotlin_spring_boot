0a4959c8b9434780933d6bfaff693d28f7fb2c41
Manuel Duarte <carlosd99m@gmail.com>
2021-10-09 15:31:50 -0600

    project setup

---
+++ src/main/kotlin/com/manuelduarte077/todo_kotlin_api/TodoKotlinApiApplication.kt
@@ -0,0 +1,11 @@
+package com.manuelduarte077.todo_kotlin_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoKotlinApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<TodoKotlinApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/manuelduarte077/todo_kotlin_api/TodoKotlinApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.manuelduarte077.todo_kotlin_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoKotlinApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
