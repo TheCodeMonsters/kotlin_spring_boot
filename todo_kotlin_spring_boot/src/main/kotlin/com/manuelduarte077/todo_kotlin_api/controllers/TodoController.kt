f5bede81aff5df8e18393e5b9045a9515f8ca9ae
Manuel Duarte <carlosd99m@gmail.com>
2021-10-09 18:20:57 -0600

    feat(api): add get and update request methods to spring boot

--- todo_kotlin_spring_boot/src/main/kotlin/com/manuelduarte077/todo_kotlin_api/controllers/TodoController.kt
+++ todo_kotlin_spring_boot/src/main/kotlin/com/manuelduarte077/todo_kotlin_api/controllers/TodoController.kt
@@ -2,17 +2,41 @@ package com.manuelduarte077.todo_kotlin_api.controllers
 
 import com.manuelduarte077.todo_kotlin_api.entities.Todo
 import com.manuelduarte077.todo_kotlin_api.entities.TodoRepository
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.hibernate.sql.Update
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
 
 @RestController
 @RequestMapping("/todos")
 class TodoController(val todoRepository: TodoRepository) {
 
+    @GetMapping
+    fun getTodos() = todoRepository.findAll()
+
+    // Consultar las tareas por medio del id de la tarea
+    @GetMapping("/{todoId}")
+    fun getTodo(@PathVariable("todoId") todoId: Long): Optional<Todo> {
+        return todoRepository.findById(todoId)
+    }
+
+    // Registar una nueva tarea
     @PostMapping
     fun newTodo(@RequestBody todo: Todo): Todo{
-        return todoRepository.save(todo);
+        return todoRepository.save(todo)
     }
+
+    // Metodo para Actualizar la tarea
+    @PutMapping("/{todoId}")
+    fun updateTodo(@PathVariable("todoId") todoId: Long, @RequestBody updateTodo: Todo): Todo? {
+        val oldTodo = todoRepository.findByIdOrNull(todoId)
+        if (oldTodo == null){
+             return oldTodo
+        }
+
+        return todoRepository.save(updateTodo)
+    }
+
+
 }
